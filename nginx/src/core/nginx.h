0669052f2d6e1a6284d56c42e7b9a4ac8121c21c
icedterminal <icedterminal@gmail.com>
2023-06-20 13:38:30 -0700

    Update NGINX and NJS

--- nginx/src/core/nginx.h
+++ nginx/src/core/nginx.h
@@ -9,8 +9,8 @@
 #define _NGINX_H_INCLUDED_
 
 
-#define nginx_version      1025001
-#define NGINX_VERSION      "1.25.1-5b8854a2f79c"
+#define nginx_version      1025002
+#define NGINX_VERSION      "1.25.2-77c1418916f7"
 #define NGINX_VER          "nginx/" NGINX_VERSION
 
 #ifdef NGX_BUILD
